746299be3f0ce5588acea222e4cbdc5ce437c746
AngelArgirov <86613309+AngelArgirov@users.noreply.github.com>
2022-01-31 15:08:04 +0000

    Delete env_sample.py

--- env_sample.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", ">wi$4=Y#',NRa9L")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://argirov98:karamelenia123@cluster0.wcoti.mongodb.net/task_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
