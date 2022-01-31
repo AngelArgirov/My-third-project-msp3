c3f5946794dec0a5b8e09ca71a7083517f367327
AngelArgirov <awargirov@gmail.com>
2022-01-31 14:58:53 +0000

    new

--- env_sample.py
+++ env_sample.py
@@ -2,6 +2,6 @@ import os
 
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "your_secret_key_here")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTER>-4g3i1.mongodb.net/<DATABASE>?retryWrites=true&w=majority")
+os.environ.setdefault("SECRET_KEY", ">wi$4=Y#',NRa9L")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://argirov98:karamelenia123@cluster0.wcoti.mongodb.net/task_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "task_manager")
